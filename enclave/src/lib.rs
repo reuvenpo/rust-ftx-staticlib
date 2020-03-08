b0494a67a140b8e5c94bcc405c63cb140068202e
Reuven Podmazo <reuven@enigma.co>
2020-03-08 22:49:55 +0200

    added implementation to the enclave crate

--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![feature(sgx_platform)]
+
+use std::os::fortanix_sgx::arch::{
+    egetkey,
+    Align512,
+};
+
+#[no_mangle] pub extern "C" fn x_add(x: i32, y: i32) -> i32 {
+    println!("{} + {} = {}; {}", x, y, x + y, egetkey(&Align512([0; 512])).unwrap().0[0]);
+    x + y
 }
